f4ba4135c5ca3a391fa80547d0c235a946d8e518
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-20 16:09:00 -0500

    manipulated photo params

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,7 +7,7 @@ class PhotosController < ApplicationController
   def create
     @photo = current_user.photos.new(photo_params)
     if @photo.save
-      redirect_to photo_path
+      redirect_to photo_path(@photo)
     else
       render :new, @photo.to_json
     end
@@ -16,14 +16,14 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
     render :show, @photo.to_json
-    redirect_to photo_path
+    redirect_to photo_path(@photo)
   end
 
   private
 
   def photo_params
-    restaurant_id = Retaurant.find_by(params[:restaurant_name])
-    params.require(:photo).permit(:image, :lng, :lat).merge(restaurant_id: restaurant_id)
+    restaurant_id = Retaurant.find_by(params[:restaurant][:name])
+    params.require(:photo).permit(:image).merge(restaurant_id: restaurant_id, lng: from_swift, lat: from_swift)
   end
 
 end
