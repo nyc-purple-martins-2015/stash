7886f26f4c8966d7277ab8ec13be732f578ebf73
Rachel Lim <email": null>
2015-11-20 11:31:09 -0500

    Add methods in tag controllers

--- app/controllers/foodtag_controller.rb
+++ app/controllers/foodtag_controller.rb
@@ -1,9 +1,17 @@
 class FoodtagController < ApplicationController
 
   def new
+    @foodtag = Foodtag.new
   end
 
   def create
+    @foodtag = Foodtag.new(foodtag_params)
+  end
+
+  private
+
+  def foodtag_params
+    params.require(:foodtag).permit(:description)
   end
 
 end
--- db/migrate/20151120021423_create_restaurants.rb
+++ db/migrate/20151120021423_create_restaurants.rb
@@ -2,6 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
       t.string   :name, null:false
+      t.decimal  :lng, {:precision => 10, :scale=>6}
+      t.decimal  :lat, {:precision => 10, :scale=>6}
 
       t.timestamps null: false
     end
