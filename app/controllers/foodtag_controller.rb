e2c65a757bbf061aed248e138184e11f09a9acbc
Rachel Lim <email": null>
2015-11-20 21:21:03 -0500

    Add foodtag parser method.

--- app/controllers/foodtag_controller.rb
+++ app/controllers/foodtag_controller.rb
@@ -7,21 +7,36 @@ class FoodtagsController < ApplicationController
   def create
     @foodtag = Foodtag.new(foodtag_params)
     if @foodtag.save
+      # Foodtag is saved after the photo is saved. This should
+      # render the user profile
+
       render json: @foodtag.to_json
     else
       render :new
     end
   end
 
+  #
   def show
     @foodtag = Foodtag.find_by(description: foodtag_params)
     render json: @foodtag.to_json
   end
 
+  def parse_foodtags(photo, foodtag_params)
+    foodtags = foodtag_params[:description].split(/[-,\/]/)
+    foodtags = foodtags.map { |tag| Foodtag.find_or_create_by(description: foodtag.strip) }.uniq
+    photo.foodtags.clear unless photo.foodtags.empty?
+
+    foodtags.each do |tag|
+      photo.foodtags << tag
+    end
+  end
+
+
   private
 
   def foodtag_params
-    params.require(:foodtag).permit(:description)
+    params.require(:foodtag).permit(:foodtags)
   end
 
 end
