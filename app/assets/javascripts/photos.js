0035a74c9f1c1504de38ad8bb31def8671c76510
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-21 13:57:40 -0500

    Commnet methods

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -13,48 +13,71 @@ $(document).ready(function(){
   //   var output = template(locals);
   // });
 
-  $(".photo_stream_container .photo_view").on(click, function(event){
-    event.preventDefault();
-    var id = $(this).attr("id")
-    var url = '/photos/' + 'id'
-    var request = $.ajax({
-      method: "get",
-      url: url,
-      data: $(this).serialize()
-    });
+  // $(".photo_stream_container .photo_view").on(click, function(event){
+  //   event.preventDefault();
+  //   var id = $(this).attr("id")
+  //   var url = '/photos/' + 'id'
+  //   var request = $.ajax({
+  //     method: "get",
+  //     url: url,
+  //     data: $(this).serialize()
+  //   });
 
-    request.done(function(data){
-      var locals = { photos: data};
-      var templateSource = $("#photo_show_tempalte").html();
-      var template = Handlebars.compile(templateSource);
-      var output = template(locals);
+  //   request.done(function(data){
+  //     var locals = { photos: data};
+  //     var templateSource = $("#photo_show_tempalte").html();
+  //     var template = Handlebars.compile(templateSource);
+  //     var output = template(locals);
 
-      $(".photo_stream_container").hide();
-      $(".my_stash").hide();
-      $(".photo_container").html(output);
-    });
-  });
+  //     $(".photo_stream_container").hide();
+  //     $(".my_stash").hide();
+  //     $(".photo_container").html(output);
+  //   });
+  // });
 
-  $(".photo_container").on(click, function(event){
-    var id = $(this).attr("id")
-    var url = '/photos/' + 'id'
-    event.preventDefault();
-    var request = $.ajax({
-      method: "get",
-      url: url,
-      data: $(this).serialize()
-    });
+  // $(".photo_container").on(click, function(event){
+  //   var id = $(this).attr("data-id")
+  //   var url = '/photos/' + 'id'
+  //   event.preventDefault();
+  //   var request = $.ajax({
+  //     method: "get",
+  //     url: url,
+  //     data: $(this).serialize()
+  //   });
 
-    request.done(function(data){
-      var source = $("#photo_show_info_template").html();
-      var template = Handlebars.compile(source);
-      var context = { photo: data };
-      var output = template(context);
+  //   request.done(function(data){
+  //     var source = $("#photo_show_info_template").html();
+  //     var template = Handlebars.compile(source);
+  //     var context = { photo: data };
+  //     var output = template(context);
 
-      $(".photo_container").hide();
-      $(".photo_info_container").html(output);
-    });
+  //     $(".photo_container").hide();
+  //     $(".photo_info_container").html(output);
+  //   });
 
-  })
+  // })
+
+  // $(".my_stash_container .stash_view").on(click, function(event){
+  //   event.preventDefault();
+  //   var id = $(this).attr("data-id")
+  //   var url = '/photos/' + 'id'
+  //   var request = $.ajax({
+  //     method: "get",
+  //     url: url,
+  //     data: $(this).serialize()
+  //   });
+
+  //   request.done(function(data){
+  //     var locals = { photos: data};
+  //     var templateSource = $("#photo_show_tempalte").html();
+  //     var template = Handlebars.compile(templateSource);
+  //     var output = template(locals);
+
+  //     $(".photo_stream_container").hide();
+  //     $(".my_stash").hide();
+  //     $(".photo_container").html(output);
+  //   });
+  // });
+
 
 })
