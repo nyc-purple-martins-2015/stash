29c8d993311228adb566f31f62b83fb434bc3253
Rachel Lim <email": null>
2015-11-21 18:49:03 -0500

    Ajax foodtags on search page.

--- app/assets/javascripts/foodtag_photo.js
+++ app/assets/javascripts/foodtag_photo.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $(".search").on("click", function(event) {
+    event.preventDefault();
+
+    hideAllContainersExcept(".foodtag_container")
+
+    $.get("/foodtags").done(function(data) {
+
+      var locals = {foodtags: data};
+      var templateSource = $("#foodtag-template").html();
+      var template = Handlebars.compile(templateSource);
+      var output = template(locals);
+      // debugger
+
+      $(".foodtag_container").html(output);
+
+    });
+  });
+});
+
