e72bfbf1007514ef48ce412d44631368f1120b47
Nick Perri <nperri6@gmail.com>
2015-11-21 13:25:56 -0500

    Delete misnamed spec files

--- spec/factories/pricetags.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :pricetag do
-    
-  end
-
-end
--- spec/models/photo_pricetag_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PhotoPricetag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
