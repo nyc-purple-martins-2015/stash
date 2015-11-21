d36388522f6ff09361b0f16838c2aac3af6f5b38
Rachel Lim <email": null>
2015-11-20 19:21:46 -0500

    Add new, create, show routes

--- app/controllers/foodtag_controller.rb
+++ app/controllers/foodtag_controller.rb
@@ -6,6 +6,16 @@ class FoodtagsController < ApplicationController
 
   def create
     @foodtag = Foodtag.new(foodtag_params)
+    if @foodtag.save
+      render json: @foodtag.to_json
+    else
+      render :new
+    end
+  end
+
+  def show
+    @foodtag = Foodtag.find_by(description: foodtag_params)
+    render json: @foodtag.to_json
   end
 
   private
