b5f3d413ffbd0a15d0e7fa431e7934520976ffef
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-21 13:56:22 -0500

    Add ajax for show individual photo, and photo info

--- app/assets/javascripts/photo_stream.js
+++ app/assets/javascripts/photo_stream.js
@@ -13,4 +13,73 @@ $(document).ready(function() {
 
     });
   });
+
+  $(document).on("click", ".photo_stream_container .photo_view", function(event){
+    event.preventDefault();
+    // debugger
+    var id = $(this).attr("data-id")
+    var url = '/photos/' + id
+    var request = $.ajax({
+      method: "get",
+      url: url,
+      data: $(this).serialize()
+    });
+
+    request.done(function(data){
+      var locals = { photo: data };
+      var templateSource = $("#photo_show_template").html();
+      var template = Handlebars.compile(templateSource);
+      var output = template(locals);
+
+      $(".photo_stream_container").hide();
+      $(".my_stash").hide();
+      $(".photo_container").html(output);
+    });
+  });
+
+  $(".photo_container").on("click", function(event){
+    event.preventDefault();
+    // debugger
+    var id = $(this).children().attr("data-id")
+    var url = '/photos/' + id
+    var request = $.ajax({
+      method: "get",
+      url: url,
+      data: $(this).serialize()
+    });
+
+    request.done(function(data){
+      var source = $("#photo_show_info_template").html();
+      var template = Handlebars.compile(source);
+      var locals = { photo: data };
+      var output = template(locals);
+      debugger
+
+      $(".photo_container").hide();
+      $(".photo_info_container").html(output);
+    });
+
+  })
+
+  // $(".my_stash_container .stash_view").on(click, function(event){
+  //   event.preventDefault();
+  //   var id = $(this).attr("data-id")
+  //   var url = '/photos/' + 'id'
+  //   var request = $.ajax({
+  //     method: "get",
+  //     url: url,
+  //     data: $(this).serialize()
+  //   });
+
+  //   request.done(function(data){
+  //     var locals = { photos: data};
+  //     var templateSource = $("#photo_show_tempalte").html();
+  //     var template = Handlebars.compile(templateSource);
+  //     var output = template(locals);
+
+  //     $(".photo_stream_container").hide();
+  //     $(".my_stash").hide();
+  //     $(".photo_container").html(output);
+  //   });
+  // });
 });
