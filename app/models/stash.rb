5c03773b9047190a20f2b5df92bb1fb5c364eef0
Rachel Lim <email": null>
2015-11-20 21:25:37 -0500

    Add current user's stash method.

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -3,12 +3,8 @@ class Stash < ActiveRecord::Base
   has_many   :photo_stashes
   has_many   :photos, through: :photo_stashes
 
-  def my_stash
-    my_stash = self.find_by( user: current_user )
-    if my_stash.count == 0
-      alert("You haven't stashed anything yet!")
-    else
-      return my_stash
-    end
+  def show_my_stash
+    self.find_by( user: current_user )
   end
+
 end
