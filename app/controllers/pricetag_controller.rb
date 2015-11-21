7e1d2dbc30cea82d2a7d357181d2a5d3de48ca88
Rachel Lim <email": null>
2015-11-20 21:23:35 -0500

    Nothing added.

--- app/controllers/pricetag_controller.rb
+++ app/controllers/pricetag_controller.rb
@@ -1,2 +1,24 @@
 class PricetagController < ApplicationController
+
+  def new
+    @pricetag = Pricetag.new
+  end
+
+  def create
+    @pricetag = Pricetag.new(pricetag_params)
+    if @pricetag.save
+      # Pricetag is saved after the photo is saved. This should
+      # render the user profile
+    else
+
+      render :new
+
+  end
+
+  #to show all price tags on search page
+  def show
+    @pricetags = Pricetag.all
+    render json: @pricetags.to_json
+  end
+
 end
