28241821f1b5f646cc53659b9b9bbd185cb27f39
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-21 16:16:53 -0500

    Add index page

--- app/controllers/stashes_controller.rb
+++ app/controllers/stashes_controller.rb
@@ -1,5 +1,15 @@
 class StashesController < ApplicationController
 
+  def index
+    @stash = Stash.find_by(user: current_user).photos
+    if @stash.any?
+      render json: @stash.to_json
+    else
+      @error ="There's no food in your stash."
+      render json: @error.to_json
+    end
+  end
+
   def new
     @stash = Stash.new
   end
@@ -13,15 +23,15 @@ class StashesController < ApplicationController
     end
   end
 
-  def show
-    stash = Stash.find_by(user: current_user).photos
-    if stash
-      render json: stash.to_json
-    else
-      @error = "There's no food in your stash."
-      render json: @error.to_json, layout: false
-    end
-  end
+  # def show
+  #   stash = Stash.find_by(user: current_user).photos
+  #   if stash
+  #     render json: stash.to_json
+  #   else
+  #     @error = "There's no food in your stash."
+  #     render json: @error.to_json, layout: false
+  #   end
+  # end
 
   private
 
