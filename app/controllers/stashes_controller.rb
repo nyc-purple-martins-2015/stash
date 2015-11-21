785d0504c940d0fceaa184c5d1cb05a8f55c3058
Rachel Lim <email": null>
2015-11-20 19:22:07 -0500

    Add new, create, show routes

--- app/controllers/stashes_controller.rb
+++ app/controllers/stashes_controller.rb
@@ -1,2 +1,31 @@
 class StashesController < ApplicationController
+
+  def new
+    @stash = Stash.new
+  end
+
+  def create
+    @stash = Stash.new(stash_params)
+    if @stash.save
+      render json: @stash.to_json
+    else
+      render :new
+    end
+  end
+
+  def show
+    @stash = my_stash
+    if @stash.any?
+      render json: @stash.to_json
+    else
+
+    end
+  end
+
+  private
+
+  def stash_params
+    params.require(:stash).merge(user_id: current_user.id)
+  end
+
 end
