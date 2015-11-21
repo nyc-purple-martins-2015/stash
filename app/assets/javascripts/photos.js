eded24b5a7c3e1cc7237771aade8c7a337331072
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-21 11:48:09 -0500

    Add show individual photo, show information of photo

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -5,4 +5,56 @@ $(document).ready(function(){
     // event.preventDefault();
 
   });
+
+  // $.get("/photos/show").done(function(data){
+  //   var locals = { photo: data };
+  //   var templateSource = $("#photo_show_template").html();
+  //   var template = Handlebars.compile(templateSource);
+  //   var output = template(locals);
+  // });
+
+  $(".photo_stream_container .photo_view").on(click, function(event){
+    event.preventDefault();
+    var id = $(this).attr("id")
+    var url = '/photos/' + 'id'
+    var request = $.ajax({
+      method: "get",
+      url: url,
+      data: $(this).serialize()
+    });
+
+    request.done(function(data){
+      var locals = { photos: data};
+      var templateSource = $("#photo_show_tempalte").html();
+      var template = Handlebars.compile(templateSource);
+      var output = template(locals);
+
+      $(".photo_stream_container").hide();
+      $(".my_stash").hide();
+      $(".photo_container").html(output);
+    });
+  });
+
+  $(".photo_container").on(click, function(event){
+    var id = $(this).attr("id")
+    var url = '/photos/' + 'id'
+    event.preventDefault();
+    var request = $.ajax({
+      method: "get",
+      url: url,
+      data: $(this).serialize()
+    });
+
+    request.done(function(data){
+      var source = $("#photo_show_info_template").html();
+      var template = Handlebars.compile(source);
+      var context = { photo: data };
+      var output = template(context);
+
+      $(".photo_container").hide();
+      $(".photo_info_container").html(output);
+    });
+
+  })
+
 })
