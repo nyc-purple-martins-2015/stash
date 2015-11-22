413305a05a7d14157763662720e169094e56b265
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-22 14:23:36 -0500

    Modify controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
     @photos = current_user.photos
     if @photos.any?
       redirect_to user_path(current_user)
-      # render json: @photos.to_json(methods: [:image_url], only: [:id]), :show
     else
       @error = {message: "You don't have any photos! Go out and eat!"}
       render :show
