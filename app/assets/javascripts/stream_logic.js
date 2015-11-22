72cfd828bff0d0b0dfb41434e4d8fd806cce77ad
Nick Perri <nperri6@gmail.com>
2015-11-22 17:16:02 -0500

    Update MVC js logic

--- app/assets/javascripts/stream_logic.js
+++ app/assets/javascripts/stream_logic.js
@@ -1,15 +1,19 @@
-var Photo = function(args){
-  this.displayed = false;
-};
+// var Photo = function(args){
+//   debugger
+//   this.displayed = false;
+//   this.created_at = args.created_at
+//   this.
 
-var Photostream = function(args){
-  this.stream = args.stream || [];
+// };
+
+var Photostream = function(photostream){
+  this.stream = photostream || [];
   this.streamShown = [];
 };
 
 Photostream.prototype.convertPhotos = function() {
   for (var i = 0; i < this.stream.length; i++) {
-    this.stream[i] = new Photo(this.stream[i]);
+    this.stream[i].displayed = false
   }
 }
 
@@ -18,32 +22,52 @@ Photostream.prototype.choosePhoto = function() {
   var nextPhoto = this.stream[i];
   this.stream.splice(i, 1);
   this.streamShown.push(nextPhoto);
+  return nextPhoto;
 }
 
-var View = function(){
-
-}
-
-View.prototype.displayPhoto = function(photo) {
-  $('.photo_container').html(photo);
+var StreamView = function(){
 
 }
 
-var Controller = function(view, photostream){
-  this.view = view;
-  this.photostream = photostream;
+StreamView.prototype.displayPhoto = function(photo) {
+
+  $('.photo_stream_container').html("<div class='photo_container' id='card'><div class='front'><img src="+photo.image_url+"></div><div class='back'>"+photo.pricetag+"</div></div>")
+  // .draggable({
+  //     revert : function(event, ui) {
+
+  //           $(this).data("uiDraggable").originalPosition = {
+  //               top : 0,
+  //               left : 0
+  //           };
+  //           return !event;
+            // that evaluate like this:
+            // return event !== false ? false : true;
+        // })
+  // .flip({
+  //       axis: "x",
+  //       trigger:'click'
+  //     });
+
 }
 
-Controller.prototype.initDisplay = function() {
+var StreamController = function(photostream){
+  this.streamview = new StreamView;
+  this.photostream = new Photostream(photostream);
+}
+
+StreamController.prototype.initDisplay = function() {
   this.photostream.convertPhotos();
-  var photo = this.phtostream.choosePhoto();
-  this.view.displayPhoto(photo);
+  var photo = this.photostream.choosePhoto();
+  this.streamview.displayPhoto(photo);
 }
 
+StreamController.prototype.addToStash = function() {
 
-Controller.prototype.addToStash = function() {
+}
 
-
+StreamController.prototype.displayNext = function() {
+  var photo = this.photostream.choosePhoto();
+  this.streamview.displayPhoto(photo);
 }
 
 
