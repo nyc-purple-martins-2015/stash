c6eb9c638aa94392ab0c3089a26d33edc202aab2
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-22 15:04:12 -0500

    Remove show route for photopricetags controller

--- app/controllers/photo_pricetags_controller.rb
+++ app/controllers/photo_pricetags_controller.rb
@@ -14,13 +14,6 @@ class PhotoPricetagsController < ApplicationController
     end
   end
 
-  def show_pricetag_photos
-    photo_pricetags = Photos.where( pricetag: photo_pricetag_params[:pricetag])
-
-    photo_pricetags = photo_pricetags.map { |photo| photo.image }
-    render json: photo_pricetags.to_json
-  end
-
   private
 
   def photo_pricetag_params
