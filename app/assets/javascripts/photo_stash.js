d603caa4a8fa383d01008262cc0039da8a2643f5
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-21 22:10:58 -0500

    Modify method

--- app/assets/javascripts/photo_stash.js
+++ app/assets/javascripts/photo_stash.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+    //click mystash button to show my stash
+  $(".my_stash").on("click", function(event){
+    event.preventDefault();
+    hideAllContainersExcept(".my_stash_container")
+    var url = $(this).attr("href");
+  $.get(url).done(function(data){
+    // debugger
+      var locals = {photos: data};
+      var templateSource = $("#mystash-template").html();
+      var template = Handlebars.compile(templateSource);
+      var output = template(locals);
+      $(".my_stash_container").html(output);
+    })
+  })
+
+
+})
