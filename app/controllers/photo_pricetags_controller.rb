5b82b3ad96f55ac3c91482544163ae262c19b944
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-22 15:35:00 -0500

    Comment out all routes and private method

--- app/controllers/photo_pricetags_controller.rb
+++ app/controllers/photo_pricetags_controller.rb
@@ -1,23 +1,23 @@
 class PhotoPricetagsController < ApplicationController
-  def new
-    @photo_pricetag = PhotoPricetag.new
-  end
+  # def new
+  #   @photo_pricetag = PhotoPricetag.new
+  # end
 
-  def create
-    @photo_pricetag = PhotoPricetag.new(photo_pricetag_params)
-    if @photo_pricetag.save
-      render json: @photo_pricetag.to_json
-      #render something?
-    else
-      render :new
-      #stay on photosaving page
-    end
-  end
+  # def create
+  #   @photo_pricetag = PhotoPricetag.new(photo_pricetag_params)
+  #   if @photo_pricetag.save
+  #     render json: @photo_pricetag.to_json
+  #     #render something?
+  #   else
+  #     render :new
+  #     #stay on photosaving page
+  #   end
+  # end
 
-  private
+  # private
 
-  def photo_pricetag_params
-    params.require(:photo_pricetag).permit(:photo, :pricetag)
-  end
+  # def photo_pricetag_params
+  #   params.require(:photo_pricetag).permit(:photo, :pricetag)
+  # end
 
 end
