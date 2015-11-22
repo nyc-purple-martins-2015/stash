c30c8065aec72c71bbc020747cb23b5b29e7b41d
rachelylim <rachel.y.lim@gmail.com>
2015-11-22 16:02:31 -0500

    Fix pricetag model.

--- app/models/pricetag.rb
+++ app/models/pricetag.rb
@@ -1,9 +1,4 @@
 class Pricetag < ActiveRecord::Base
   has_one :photo_pricetags
-<<<<<<< 93285d2391df3d6dc310f6904a9adf89291b907e
   has_many :photos, through: :photo_pricetags
-=======
-  has_one :photos, through: :photo_pricetags
-
->>>>>>> Cleanup pricetag model, seeds, and controller.
 end
