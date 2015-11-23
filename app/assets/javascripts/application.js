1119aa9df1fa4168f9571489bd1c0e10148669d9
rachelylim <rachel.y.lim@gmail.com>
2015-11-23 12:58:50 -0500

    Add Google Maps javascript to initialize map and pinpoint user location.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,61 @@
 //   $('div.container > [class*="container"]').hide();
 //   $(divNotHidden).show()
 // };
+
+// initialize();
+
+// function initialize() {
+//   google.maps.event.addDomListener(window, 'load', function() {
+//     codeAddress('New York');
+// });
+// }
+
+$(document).ready(function() {
+
+  function initialize() {
+
+    var myOptions = {
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+    // Get's user's current location
+    if(navigator.geolocation) {
+      browserSupportFlag = true;
+      navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+  
+      map.setCenter(initialLocation);
+
+      var marker = new google.maps.Marker({
+        position: initialLocation,
+        map: map,
+        title: 'Hello World!'
+      });
+    
+      }, function() {
+        handleNoGeolocation(browserSupportFlag);
+      });
+    }
+    // Browser doesn't support Geolocation
+    else {
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
+    }
+
+    function handleNoGeolocation(errorFlag) {
+      if (errorFlag == true) {
+        alert("Geolocation service failed.");
+        initialLocation = newyork;
+      } else {
+        alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+        initialLocation = siberia;
+      }
+      map.setCenter(initialLocation);
+    }
+  }
+
+  initialize();
+
+});
