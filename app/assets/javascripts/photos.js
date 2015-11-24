d078539a8b3bb85744feb332eab4de506bf55fde
Matthew LeFurge <email": null>
2015-11-24 13:36:00 -0500

    Add javascript for photo  preview

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -16,5 +16,27 @@ $(document).ready(function(){
     document.getElementById("lat_field").value = position.coords.latitude
     document.getElementById('lng_field').value = position.coords.longitude
   }
+
+  $("#photo_image").on('change',function() {
+    if (typeof (FileReader) != "undefined") {
+
+      var photo_preview = $('#photo_preview');
+      photo_preview.empty();
+
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $("<img />", {
+          "src": e.target.result,
+          "id": "photo_preview"
+        }).appendTo(photo_preview);
+      }
+      photo_preview.show();
+      reader.readAsDataURL($(this)[0].files[0]);
+    } else {
+      alert("This broswer does not support FileREader.")
+    }
+
+  });
+
 });
 
