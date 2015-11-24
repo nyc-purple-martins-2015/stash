9bb94a3884f7ecaf64659e02ad8422a1c4d21e82
Rachel Lim <email": null>
2015-11-24 12:45:41 -0500

    Fix new photo bug AGAIN...

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,16 +12,11 @@ class Photo < ActiveRecord::Base
   delegate          :url, to: :image, prefix: true
 
   has_attached_file :image,
-                    #url: "/system/:hash.:extension",
-                    #hash_secret: "abc123",
-                    #preserve_files: "true",
-                    styles: { thumb: ["64x64#", :jpg],
-                              original: ['500x500>', :jpg] },
-                    convert_options: { thumb: "-quality 75 -strip",
-                                       original: "-quality 85 -strip" }
+                    :styles => { :original => "1024x1024#",
+                                 :thumb => "150x150>" }
   validates_attachment :image, presence: true,
-                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
-                       size: { in: 0..500.kilobytes }
+                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+                       # size: { in: 0..500.kilobytes }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
