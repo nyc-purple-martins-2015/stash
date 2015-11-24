869a758150e7a9314ced8380b94f5159fbb86727
Matthew LeFurge <email": null>
2015-11-24 11:51:36 -0500

    Add search method

--- app/models/foodtag.rb
+++ app/models/foodtag.rb
@@ -1,4 +1,9 @@
 class Foodtag < ActiveRecord::Base
   has_many :foodtag_photos
   has_many :photos, through: :foodtag_photos
+
+  def self.search(description)
+    description = description.downcase
+    where("description LIKE ?", "%#{description}%")
+  end
 end
