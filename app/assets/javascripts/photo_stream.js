2fed15e7920837b522634d431fbebc12ab8e18ed
Matthew LeFurge <email": null>
2015-11-23 21:35:10 -0500

    Add photostream logic for index

--- app/assets/javascripts/photo_stream.js
+++ app/assets/javascripts/photo_stream.js
@@ -1,109 +1,149 @@
-// $(document).ready(function() {
-
-//   //test page to load up all images, click each image, and show image infos
-//   $.get("/home/show").done(function(data){
-//     var locals = { photos: data};
-//     var templateSource = $("#photostream-template").html();
-//     var template = Handlebars.compile(templateSource);
-//     var output = template(locals);
-//     // console.log(output);
-//     // debugger
-//     $(" .photo_stream_container").html(output);
-
-//     });
-
-  //click home button to show photo stream
-  // $(".home").on("click", function(event){
-  //   event.preventDefault();
-
-  //   hideAllContainersExcept(".photo_stream_container")
-
-  //   $.get("/home/index").done(function(data){
-  //   var locals = { photos: data};
-  //   var templateSource = $("#photostream-template").html();
-  //   var template = Handlebars.compile(templateSource);
-  //   var output = template(locals);
-  //   // debugger
-  //   $(".photo_stream_container").html(output);
-
-  //   });
-
-  // });
+// jQueryMobile-SwipeUpDown
+// ----------------------------------
+//
+// Copyright (c)2012 Donnovan Lewis
+// Distributed under MIT license
+//
+// https://github.com/blackdynamo/jquerymobile-swipeupdown
 
 
-  //show one photo from photostream when it is clicked
-  $(document).on("click", ".photo_stream_container .photo_view", function(event){
-    event.preventDefault();
-    // debugger
-    hideAllContainersExcept(".photo_container")
-    var id = $(this).attr("data-id")
-    var url = '/photos/' + id
-    var request = $.ajax({
-      method: "get",
-      url: url,
-      data: $(this).serialize()
+
+
+
+$(document).ready(function() {
+// initializes touch and scroll events
+    var supportTouch = $.support.touch,
+        scrollEvent = "touchmove scroll",
+        touchStartEvent = supportTouch ? "touchstart" : "mousedown",
+        touchStopEvent = supportTouch ? "touchend" : "mouseup",
+        touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
+
+    // handles swipeup and swipedown
+    $.event.special.swipeupdown = {
+        setup: function () {
+            var thisObject = this;
+            var $this = $(thisObject);
+
+            $this.bind(touchStartEvent, function (event) {
+                var data = event.originalEvent.touches ?
+                        event.originalEvent.touches[ 0 ] :
+                        event,
+                    start = {
+                        time: (new Date).getTime(),
+                        coords: [ data.pageX, data.pageY ],
+                        origin: $(event.target)
+                    },
+                    stop;
+
+                function moveHandler(event) {
+                    if (!start) {
+                        return;
+                    }
+
+                    var data = event.originalEvent.touches ?
+                        event.originalEvent.touches[ 0 ] :
+                        event;
+                    stop = {
+                        time: (new Date).getTime(),
+                        coords: [ data.pageX, data.pageY ]
+                    };
+
+                    // prevent scrolling
+                    if (Math.abs(start.coords[1] - stop.coords[1]) > 10) {
+                        event.preventDefault();
+                    }
+                }
+
+                $this
+                    .bind(touchMoveEvent, moveHandler)
+                    .one(touchStopEvent, function (event) {
+                        $this.unbind(touchMoveEvent, moveHandler);
+                        if (start && stop) {
+                            if (stop.time - start.time < 1000 &&
+                                Math.abs(start.coords[1] - stop.coords[1]) > 30 &&
+                                Math.abs(start.coords[0] - stop.coords[0]) < 75) {
+                                start.origin
+                                    .trigger("swipeupdown")
+                                    .trigger(start.coords[1] > stop.coords[1] ? "swipeup" : "swipedown");
+                            }
+                        }
+                        start = stop = undefined;
+                    });
+            });
+        }
+    };
+
+//Adds the events to the jQuery events special collection
+    $.each({
+        swipedown: "swipeupdown",
+        swipeup: "swipeupdown"
+    }, function (event, sourceEvent) {
+        $.event.special[event] = {
+            setup: function () {
+                $(this).bind(sourceEvent, $.noop);
+            }
+        };
+        //Adds new events shortcuts
+        $.fn[ event ] = function( fn ) {
+            return fn ? this.bind( event, fn ) : this.trigger( event );
+        };
+        // jQuery < 1.8
+        if ( $.attrFn ) {
+            $.attrFn[ event ] = true;
+        }
     });
 
-    request.done(function(data){
-      var locals = { photo: data };
-      var templateSource = $("#photo_show_template").html();
-      var template = Handlebars.compile(templateSource);
-      var output = template(locals);
 
+  if ($('.photo_stream_container').length !== 0) {
+    var cont = new StreamController($('.photo_stream_container').data('photostream'));
+    cont.photostream.convertPhotos();
+    cont.initDisplay();
 
-      $(".photo_container").html(output);
-    });
-  });
+    $('.photo_stream_container').on('swipeleft', '.photo_container', function(){
+      $(this).fadeOut('600').animate({
+          left: -300
+        },
+        {
+          duration: 600, queue: false,
+          complete: function() {
+            cont.displayNext();
+          },
+        }
+      );
+      console.log("swiped left");
 
-
-  // //show photo info when it is clicked
-  $(".photo_container").on("click", function(event){
-    event.preventDefault();
-    // hideAllContainersExcept(".photo_info_container")
-    // debugger
-    var id = $(this).children().attr("data-id")
-    var url = '/photos/' + id
-    var request = $.ajax({
-      method: "get",
-      url: url,
-      data: $(this).serialize()
     });
 
-    request.done(function(data){
-      var source = $("#photo_show_info_template").html();
-      var template = Handlebars.compile(source);
-      var locals = { photo: data };
-      var output = template(locals);
-      // debugger
+    $('.photo_stream_container').on('swiperight', '.photo_container', function(){
 
-      $(".photo_info_container").html(output);
+      $(this).fadeOut('600').animate({
+          left: 300
+        },
+        {
+          duration: 600, queue: false,
+          complete: function() {
+            cont.displayNext();
+          },
+        }
+      );
+
+      console.log('swiped right');
     });
 
-  })
+    $('.photo_stream_container').on('swipeupdown', '.photo_container', function(){
+        $(this).fadeOut('600').animate({
+          top: 200
+        },
+        {
+          duration: 600, queue: false,
+          complete: function() {
+            cont.displayNext();
+          },
+        }
+      );
+      console.log('swiped down');
+      // cont.displayNext();
 
-  //show photo from my stash when it is clicked
-  $(".my_stash_container .stash_view").on("click", function(event){
-    event.preventDefault();
-
-    hideAllContainersExcept(".photo_container")
-
-    var id = $(this).children().attr("data-id")
-    var url = '/photos/' + 'id'
-    var request = $.ajax({
-      method: "get",
-      url: url,
-      data: $(this).serialize()
     });
-
-    request.done(function(data){
-      var locals = { photo: data};
-      var templateSource = $("#photo_show_tempalte").html();
-      var template = Handlebars.compile(templateSource);
-      var output = template(locals);
-
-      $(".photo_container").html(output);
-    });
-  });
-
-
-// });
+  };
+});
