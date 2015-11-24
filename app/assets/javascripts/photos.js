5a1f87fe97dc2f446d47b96b8f3558e675bf1e51
Rachel Lim <email": null>
2015-11-24 16:23:30 -0500

    Implement ability to flip photos to reveal photo information.

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -38,12 +38,23 @@ $(document).ready(function(){
 
   });
 
-  $("#polaroid > figure").on("click", function(event) {
-    // debugger
-    $(this).toggleClass("#polaroid > figure.back")
+  $(".flippable").click(function() {
+    var front = $(this).children(".front");
+    var back = $(this).children(".back");
+    var toHide = front.is(':visible') ? front : back;
+    var toShow = back.is(':visible') ? front : back;
 
+    toHide.removeClass('flip in').addClass('flip out').hide();
+    toShow.removeClass('flip out').addClass('flip in').show();
 
   })
+
+  // $("#polaroid > figure").on("click", function(event) {
+  //   // debugger
+  //   $(this).toggleClass("#polaroid > figure.back")
+
+
+  // })
 
 
 
