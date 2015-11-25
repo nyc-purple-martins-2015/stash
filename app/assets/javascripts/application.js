336ce4a8f2f5340d23465331ae861a6ee57f68c5
Rachel Lim <email": null>
2015-11-25 11:09:31 -0500

    Request restaurant name, address, phone, website, and rating from Google Place API.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,63 +19,61 @@
 $(document).ready(function() {
 // autocomplete place details
 
-if (document.getElementById("map")) {
+  if (document.getElementById("map")) {
 
-  (function initialize (){
-    var defaultLatLng = new google.maps.LatLng(40.7127, -74.0059)
+    (function initialize (){
+      var defaultLatLng = new google.maps.LatLng(40.7127, -74.0059)
 
-    //show current location
-    if ( navigator.geolocation ) {
-      function success(pos) {
-        drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      //show current location
+      if ( navigator.geolocation ) {
+        function success(pos) {
+          drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);
+        }
+          navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+        } else {
+          drawMap(defaultLatLng);  // No geolocation support, show default map
       }
-      function fail(error) {
-          drawMap(defaultLatLng);
+
+      //function to make a map and drop a pin
+      function drawMap(latlng) {
+        var myOptions = {
+          zoom: 18,
+          center: latlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+        });
+      // autofill function
+        var input = document.getElementById('searchTextField');
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        autocomplete.bindTo('bounds', map);
+        var infowindow = new google.maps.InfoWindow();
+
+        autocomplete.addListener('place_changed', function() {
+            infowindow.close();
+            var place = autocomplete.getPlace();
+            $('#restaurant_name').val(place.name);
+            $('#restaurant_rating').val(place.rating);
+            $('#restaurant_address').val(place.vicinity);
+            $('#restaurant_phone').val(place.formatted_phone_number);
+            $('#restaurant_website').val(place.website);
+            // debugger
+
+        });
+        width = screen.width.toString(); + "px"
+        height = (screen.height/4).toString() + "px"
+        document.getElementById("map").style.width = width;
+        document.getElementById("map").style.height = height;
       }
-        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
-      } else {
-        drawMap(defaultLatLng);  // No geolocation support, show default map
-    }
-
-    //function to make a map and drop a pin
-    function drawMap(latlng) {
-      var myOptions = {
-        zoom: 18,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-      var marker = new google.maps.Marker({
-          position: latlng,
-          map: map,
-      });
-    // autofill function
-      var input = document.getElementById('searchTextField');
-      console.log(input);
-
-      var autocomplete = new google.maps.places.Autocomplete(input);
-
-      autocomplete.bindTo('bounds', map);
-      var infowindow = new google.maps.InfoWindow();
-
-      autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          var place = autocomplete.getPlace();
-          $('#photo_restaurant').val(place.name);
-
-          console.log(place);
-      });
-      width = screen.width.toString(); + "px"
-      height = (screen.height/4).toString() + "px"
-      document.getElementById("map").style.width = width;
-      document.getElementById("map").style.height = height;
-    }
-
-}) ();
-
-}
-
-
-//document ready end
+    }) ();
+  }
 });
