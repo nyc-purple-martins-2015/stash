0af155563a5be589d7cfb64ce8ab2b545cdf9204
Rachel Lim <email": null>
2015-11-25 11:13:13 -0500

    Add restaurant info attributes in phone migration.

--- db/migrate/20151120021149_create_photos.rb
+++ db/migrate/20151120021149_create_photos.rb
@@ -2,10 +2,14 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
 
-      t.string      :restaurant
-      t.string      :restaurant_address
       t.references  :user, null: false, index: true
       t.string      :dish_name
+
+      t.string      :restaurant_name
+      t.string      :restaurant_rating
+      t.string      :restaurant_address
+      t.string      :restaurant_phone
+      t.string      :restaurant_website
 
       t.decimal     :lng, {:precision => 10, :scale=>6}
       t.decimal     :lat, {:precision => 10, :scale=>6}
