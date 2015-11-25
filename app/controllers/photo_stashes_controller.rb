44587fbff10fa7bbae0cc5562cac9207a2eb8f39
Rachel Lim <email": null>
2015-11-25 12:08:30 -0500

    Delete unneccessary code.

--- app/controllers/photo_stashes_controller.rb
+++ app/controllers/photo_stashes_controller.rb
@@ -1,6 +1,5 @@
 class PhotoStashesController < ApplicationController
   def destroy
-    # byebug
     @stashed_photo = PhotoStash.find_by(stash_id: current_user.stash.id, photo_id: params[:id])
     @stashed_photo.destroy
     redirect_to user_stashes_path(current_user)
