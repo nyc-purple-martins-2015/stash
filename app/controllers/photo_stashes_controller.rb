f66e76c86445b3a335fb408786572da3e688595a
Rachel Lim <email": null>
2015-11-25 12:06:55 -0500

    Add photo_stash controller to delete stashed photo in user stash.

--- app/controllers/photo_stashes_controller.rb
+++ app/controllers/photo_stashes_controller.rb
@@ -1,7 +1,8 @@
 class PhotoStashesController < ApplicationController
   def destroy
-    @stashed_photo = Photo.find(params[:id])
-    byebug
-    @stash_photo.destroy
+    # byebug
+    @stashed_photo = PhotoStash.find_by(stash_id: current_user.stash.id, photo_id: params[:id])
+    @stashed_photo.destroy
+    redirect_to user_stashes_path(current_user)
   end
 end
