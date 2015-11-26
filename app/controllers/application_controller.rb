7a759537e4267f02418d0b4c611c5fad0eae3d91
Rachel Lim <email": null>
2015-11-25 19:14:23 -0500

    Add welcoming header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :image_url, :user_stash_count
 
   def current_user
+    # @current_user = User.first
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
