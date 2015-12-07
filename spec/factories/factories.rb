ea8576360e8658049dea13a088912a0b4330340d
Rachel Lim <rachel.y.lim@gmail.com>
2015-12-06 21:38:03 -0500

    Move factories into one factory file.

---
+++ spec/factories/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory(:user) do
+    provider { Faker::Company.name }
+    uid { Faker::Number.number(6) }
+    name { Faker::Name.name }
+  end
+end
+
+FactoryGirl.define do
+  factory(:foodtag) do
+    description { Faker::Name.name }
+  end
+end
+
+FactoryGirl.define do
+  factory(:foodtag_photo) do
+    photo
+    foodtag
+  end
+end
