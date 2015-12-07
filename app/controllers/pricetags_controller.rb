9205340ac3a4118f89406eee35c8a783d0859e67
Rachel Lim <rachel.y.lim@gmail.com>
2015-12-07 11:21:50 -0500

    Remove #index. Pricetags are shown on Foodtags index.

--- app/controllers/pricetags_controller.rb
+++ app/controllers/pricetags_controller.rb
@@ -1,9 +1,5 @@
 class PricetagsController < ApplicationController
 
-  def index
-    @pricetags = Pricetag.all
-  end
-
   def show
     @photo_pricetags = Pricetag.find(params[:id]).photos
   end
---
+++ spec/controllers/pricetags_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe PricetagsController do
+  let(:user)  { FactoryGirl.create(:user) }
+  let(:sample_photo) { Photo.create(restaurant_name: "New Restaurant",
+                      restaurant_address: "123 New Street",
+                      user: user,
+                      image: Rack::Test::UploadedFile.new(Rails.root.join('seed_photos', 'fish.jpg'), 'image/jpeg'),
+                      dish_name: "delicious",
+                      lng: 12.2132,
+                      lat: 13.4241) }
+
+  let(:sample_pricetag) { FactoryGirl.create(:pricetag) }
+  # let(:sample_photo_pricetag) { FactoryGirl.create(photo: sample_photo, pricetag: sample_pricetag)}
+  it '#show the photos with specified pricetag' do
+    PhotoPricetag.create(photo: sample_photo, pricetag: sample_pricetag)
+    get :show, id: sample_pricetag.id
+    expect(assigns(:photo_pricetags).first).to be_a_kind_of Photo
+    expect(assigns(:photo_pricetags).first.id).to eq sample_photo.id
+  end  
+
+
+end
